657a65d8ab183532711259f2bcf318183d872479
David Snyder <davidgraygs4@gmail.com>
2020-04-20 09:12:06 -0400

    Updates

---
+++ precache-manifest.6b4303a8aa91132a281079a157958901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a83a5728752a1c0c7be31becbe44039",
+    "url": "/React-calculator/index.html"
+  },
+  {
+    "revision": "bc51647ad47860526020",
+    "url": "/React-calculator/static/css/main.6a03f4ec.chunk.css"
+  },
+  {
+    "revision": "e293aae5982425742cb2",
+    "url": "/React-calculator/static/js/2.c5b248fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React-calculator/static/js/2.c5b248fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc51647ad47860526020",
+    "url": "/React-calculator/static/js/main.b6033d9b.chunk.js"
+  },
+  {
+    "revision": "7f8f65dfd4bc2583ff6e",
+    "url": "/React-calculator/static/js/runtime-main.b848423a.js"
+  }
+]);
